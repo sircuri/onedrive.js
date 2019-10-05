caae1eabcf5261f0d235d803d6c1124e40b01db7
Arjen <arjen@vanefferenonline.nl>
2019-10-05 21:44:34 +0200

    Add Mutex object

--- src/index.ts
+++ src/index.ts
@@ -63,6 +63,31 @@ function* dirsAndFiles(baseDir: string, dir: string, options?: { pattern?: RegEx
     }
 }
 
+export class Mutex {
+    private mutex = Promise.resolve();
+
+    lock(): PromiseLike<() => void> {
+        let begin: (unlock: () => void) => void = unlock => { };
+
+        this.mutex = this.mutex.then(() => {
+            return new Promise(begin);
+        });
+
+        return new Promise(res => {
+            begin = res;
+        });
+    }
+
+    async dispatch(fn: (() => Promise<any>) | (() => PromiseLike<any>)): Promise<any> {
+        const unlock = await this.lock();
+        try {
+            return await Promise.resolve(fn());
+        } finally {
+            unlock();
+        }
+    }
+}
+
 (async () => {
 
     const config = new Config();
